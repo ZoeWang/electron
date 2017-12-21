8fbc576782d2935aae4fd1a90e3c644d678c1fc7
王秀芳 <wangxf01@mail.open.com.cn>
2017-12-21 10:38:49 +0800

    添加命令

--- README.md
+++ README.md
@@ -31,6 +31,14 @@ npm start
 
 Note: If you're using Linux Bash for Windows, [see this guide](https://www.howtogeek.com/261575/how-to-run-graphical-linux-desktop-applications-from-windows-10s-bash-shell/) or use `node` from the command prompt.
 
+```
+## 打包 win
+npm run-script packageWin
+
+## 打包 mac
+npm run-script packageDarwin
+```
+
 ## Resources for Learning Electron
 
 - [electron.atom.io/docs](http://electron.atom.io/docs) - all of Electron's documentation
@@ -43,3 +51,5 @@ Note: If you're using Linux Bash for Windows, [see this guide](https://www.howto
 ## License
 
 [CC0 1.0 (Public Domain)](LICENSE.md)
+
+
--- main.js
+++ main.js
@@ -13,23 +13,30 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  // mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  // // and load the index.html of the app.
+  // mainWindow.loadURL(url.format({
+  //   pathname: path.join(__dirname, 'index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // // Open the DevTools.
+  // // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+  // // Emitted when the window is closed.
+  // mainWindow.on('closed', function () {
+  //   // Dereference the window object, usually you would store windows
+  //   // in an array if your app supports multi windows, this is the time
+  //   // when you should delete the corresponding element.
+  //   mainWindow = null
+  // })
+
+  // 添加创建好的web 站点
+  mainWindow = new BrowserWindow({width: 1200, height: 800,frame: false })
+  mainWindow.loadURL('http://mgr.shixunbao.cn/#/login')
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
 }
